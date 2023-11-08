00d6180f39c536c2b730890223dcbdfa2a2f475a
gun1kim <obkh31@naver.com>
2023-11-08 22:20:16 +0900

    231108

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,9 +1,46 @@
 package hellojpa;
 
-import jakarta.persistence.Persistence;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
+        tx.begin();
+
+        try {
+            // 회원 등록
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+//            em.persist(member);
+
+            // 회원 수정
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HelloJpa");
+
+            // 회원 삭제
+//            em.remove(findMember);
+
+            List<Member> result = em.createQuery("select m from Member m ", Member.class).getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.getName() = " + member.getName());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
     }
 }
